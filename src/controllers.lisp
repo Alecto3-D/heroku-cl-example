9d63cfff603f89ba0a60f7a9dc0bafecb7afa7c8
Marc Kuo <kuomarc2@gmail.com>
2012-04-11 18:15:10 +0200

    add dispatch for new movie to controller

--- src/controllers.lisp
+++ src/controllers.lisp
@@ -3,4 +3,5 @@
 (setq *dispatch-table*
       (list
        (create-regex-dispatcher "^/index" 'controller-index)
+       (create-regex-dispatcher "^/new" 'controller-new)
        (create-static-file-dispatcher-and-handler "/site.css" "static/application.css")))
