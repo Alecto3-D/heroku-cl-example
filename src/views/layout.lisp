41d9cc2d2823e2d183e2770fb2d8c557eecb7ab1
Marc Kuo <kuomarc2@gmail.com>
2012-04-11 17:00:09 +0200

    add standard-page macro

--- src/views/layout.lisp
+++ src/views/layout.lisp
@@ -1,5 +1,16 @@
 (in-package :example)
 
+(defmacro standard-page ((&key title) &body body)
+  `(with-html-output-to-string (*standard-output* nil :prologue t :indent t)
+     (:html :xmlns "http://www.w3.org/1999/xhtml" :xml\:lang "en" :lang "en"
+	    (:head
+	     (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
+	     (:title ,title)
+	     (:link :type "text/css" :rel "stylesheet" :href "static/application.css"))
+	    (:body
+	     (:h1 :class "title" "Rotten Potatoes!")
+	     ,@body))))
+
 (hunchentoot:define-easy-handler (hello-sbcl :uri "/") ()
   (cl-who:with-html-output-to-string (s)
     (:html
