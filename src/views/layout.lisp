44318f3ff01bde7efd354dedf867644ada3cfcf3
Marc Kuo <kuomarc2@gmail.com>
2012-04-11 17:12:45 +0200

    remove old code

--- src/views/layout.lisp
+++ src/views/layout.lisp
@@ -6,29 +6,7 @@
 	    (:head
 	     (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
 	     (:title ,title)
-	     (:link :type "text/css" :rel "stylesheet" :href "static/application.css"))
+	     (:link :type "text/css" :rel "stylesheet" :href "/site.css"))
 	    (:body
 	     (:h1 :class "title" "Rotten Potatoes!")
 	     ,@body))))
-
-(hunchentoot:define-easy-handler (hello-sbcl :uri "/") ()
-  (cl-who:with-html-output-to-string (s)
-    (:html
-     (:head
-      (:title "Heroku CL Example App"))
-     (:body
-      (:h1 "Heroku CL Example App")
-      (:h3 "Using")
-      (:ul
-       (:li (format s "~A ~A" (lisp-implementation-type) (lisp-implementation-version)))
-       (:li (format s "Hunchentoot ~A" hunchentoot::*hunchentoot-version*))
-       (:li (format s "CL-WHO")))
-      (:div
-       (:a :href "static/lisp-glossy.jpg" (:img :src "static/lisp-glossy.jpg" :width 100)))
-      (:div
-       (:a :href "static/hello.txt" "hello"))
-      (:h3 "App Database")
-      (:div
-       (:pre "SELECT version();"))
-      (:div (format s "~A" (postmodern:with-connection (db-params)
-			     (postmodern:query "select version()"))))))))
