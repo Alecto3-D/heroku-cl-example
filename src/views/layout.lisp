0f2af08b4935e66d29a691918fdccffd5b1e17f2
Marc Kuo <kuomarc2@gmail.com>
2012-04-11 17:30:35 +0200

    add main div to body

--- src/views/layout.lisp
+++ src/views/layout.lisp
@@ -2,11 +2,12 @@
 
 (defmacro standard-page (&body body)
   `(with-html-output-to-string (*standard-output* nil :prologue t :indent t)
-     (:html :xmlns "http://www.w3.org/1999/xhtml" :xml\:lang "en" :lang "en"
-	    (:head
-	     (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
-	     (:title "Rotten Potatoes!")
-	     (:link :type "text/css" :rel "stylesheet" :href "/site.css"))
-	    (:body
-	     (:h1 :class "title" "Rotten Potatoes!")
-	     ,@body))))
+     (:html
+      (:head
+       (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
+       (:title "Rotten Potatoes!")
+       (:link :type "text/css" :rel "stylesheet" :href "/site.css"))
+      (:body
+       (:h1 :class "title" "Rotten Potatoes!")
+       (:div :id "main"
+	     ,@body)))))
