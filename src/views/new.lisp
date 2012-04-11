d962f53d25b25e5ef569193f8c8e0ea911a9ca80
Marc Kuo <kuomarc2@gmail.com>
2012-04-11 18:48:40 +0200

    change to labels and fix selector

--- src/views/new.lisp
+++ src/views/new.lisp
@@ -4,15 +4,15 @@
   (standard-page
     (:h1 "Create new movie")
     (:form :action "/movies" :method "post"
-	   (:p "Title")
+	   (:label "Title")
 	   (:input :name "movie-title" :size "30" :type "text")
-	   (:p "Rating")
+	   (:label "Rating")
 	   (:select :name "movie-rating"
-		    (:option :value "G")
-		    (:option :value "PG")
-		    (:option :value "PG-13")
-		    (:option :value "R")
-		    (:option :value "NC-17")))))
+		    (:option :value "G" "G")
+		    (:option :value "PG" "PG"
+		    (:option :value "PG-13" "PG-13")
+		    (:option :value "R" "R")
+		    (:option :value "NC-17" "NC-17"))))))
 
     
 
