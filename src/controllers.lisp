240cf10cee812fe911f39831985f3de1a17e37ad
Marc Kuo <kuomarc2@gmail.com>
2012-04-12 15:22:57 +0200

    adjust to be consistent with rails version

--- src/controllers.lisp
+++ src/controllers.lisp
@@ -2,6 +2,6 @@
 
 (setq *dispatch-table*
       (list
-       (create-regex-dispatcher "^/index" 'controller-index)
-       (create-regex-dispatcher "^/new" 'controller-new)
+       (create-regex-dispatcher "(^/movies$|^/$)" 'controller-index)
+       (create-regex-dispatcher "^/movies/new" 'controller-new)
        (create-static-file-dispatcher-and-handler "/site.css" "static/application.css")))
