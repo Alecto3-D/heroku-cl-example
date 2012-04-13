b1fe0db34e80e2acaeda3687c78d71ced47f806c
Marc Kuo <kuomarc2@gmail.com>
2012-04-13 18:50:50 +0200

    add unique id to movie object -- CRUD operators

--- src/model.lisp
+++ src/model.lisp
@@ -1,14 +1,49 @@
 (in-package :example)
 
 (defclass movie ()
-  ((title :col-type string :initarg :title :reader movie-title)
+  ((id :col-type serial :initarg :id)
+   (title :col-type string :initarg :title :reader movie-title)
    (rating :col-type string :initarg :rating :accessor movie-rating)
    (release-date :col-type date :initarg :release-date :accessor movie-release-date))
   (:metaclass dao-class)
-  (:keys title))
+  (:keys id))
 
-(defun init-table (type)
-  "Creates table if it doesn't exist already. Expects symbol."
+(defun movie-init-table ()
+  "Creates table if it doesn't exist already."
   (with-connection (db-params)
-    (unless (table-exists-p type)
-      (execute (dao-table-definition type)))))
+    (when (table-exists-p 'movie)
+      (query "DROP TABLE movie"))
+    (execute (dao-table-definition 'movie))))
+
+;; Create
+
+(defmacro movie-create (&rest args)
+  `(with-connection (db-params)
+     (make-dao 'movie ,@args)))
+
+;; Read
+
+(defun movie-get-all ()
+  (with-connection (db-params)
+    (select-dao 'movie)))
+
+(defun movie-get (id)
+  (with-connection (db-params)
+    (get-dao 'movie id)))
+
+(defmacro movie-select (test &optional sort)
+  `(with-connection (db-params)
+     (select-dao 'movie ,test ,sort)))
+
+;; Update
+
+(defun movie-update (movie)
+  (with-connection (db-params)
+    (update-dao movie)))
+
+;; Delete
+
+(defun movie-delete (movie)
+  (with-connection (db-params)
+    (delete-dao movie)))
+  
