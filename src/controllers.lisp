00a6362053d0caac76504159adccbdb939163ce4
Marc Kuo <kuomarc2@gmail.com>
2012-04-23 22:53:05 +0200

    redirect / to /movies -- convention of RESTful URI

--- src/controllers.lisp
+++ src/controllers.lisp
@@ -2,10 +2,14 @@
 
 (setq *dispatch-table*
       (list
-       (create-regex-dispatcher "(^/movies$|^/$)" 'controller-index)
+       (create-regex-dispatcher "^/$" 'controller-to-index)
+       (create-regex-dispatcher "^/movies$" 'controller-index)
        (create-regex-dispatcher "^/movies/new" 'controller-new)
        (create-regex-dispatcher "^/add-movie" 'controller-add-movie)
        (create-static-file-dispatcher-and-handler "/site.css" "static/application.css")))
+
+(defun controller-to-index ()
+  (redirect "/movies"))
 
 (defun controller-add-movie ()
   "Form processer for adding new movie."
