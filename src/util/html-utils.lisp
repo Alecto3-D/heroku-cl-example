e552d7f86c4ae8c310711ae4084c29d79f20ecdb
Marc Kuo <kuomarc2@gmail.com>
2012-04-23 22:58:01 +0200

    added movies-path function to return the associated URI

--- src/util/html-utils.lisp
+++ src/util/html-utils.lisp
@@ -5,6 +5,10 @@
     (with-output-to-string (s)
       (format s "~4,'0d-~2,'0d-~2,'0d" year month day))))
 
+(defun movies-path (movie)
+  "Given a <Movie>, return the RESTful URI -- e.g. 'movies/3'"
+  (concatenate 'string "movies/" (write-to-string (movie-id movie))))
+
 (defun all-movie-rows ()
   (dolist (movie (movie-get-all))
     (htm
@@ -12,7 +16,7 @@
       (:td (fmt "~a" (movie-title movie)))
       (:td (fmt "~a" (movie-rating movie)))
       (:td (fmt "~a" (print-date (movie-release-date movie))))
-      (:td (:a :href (concatenate 'string "movies/" (write-to-string (movie-id movie)))
+      (:td (:a :href (movies-path movie)
 	       (fmt "More info about ~A" (movie-title movie))))))))
 
 (def-internal-macro selector-form (name options)
