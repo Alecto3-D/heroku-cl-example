9e80f8052f997251203b0de861868cf62e221fd4
Marc Kuo <kuomarc2@gmail.com>
2012-04-26 17:11:34 -0700

    fix static links to edit and delete with id

--- src/views/show.lisp
+++ src/views/show.lisp
@@ -1,12 +1,13 @@
 (in-package :example)
 
 (defun controller-show ()
-  (let ((movie (movie-get (get-id-from-uri))))
+  (let* ((id (get-id-from-uri))
+	 (movie (movie-get id)))
     (standard-page
       (:h2 (fmt "Details about ~A" (movie-title movie)))
       (:ul :id "details"
 	   (:li (fmt "Rating: ~A" (movie-rating movie)))
 	   (:li (fmt "Released on: ~A" (print-date (movie-release-date movie)))))
-      (:a :href "2/edit" "Edit movie")(:br)
-      (:a :href "2/delete" "Delete movie")(:br)
+      (:a :href (concatenate 'string id "/edit") "Edit movie")(:br)
+      (:a :href (concatenate 'string id "/delete") "Delete movie")(:br)
       (:a :href "/movies" "Back to movies list"))))
