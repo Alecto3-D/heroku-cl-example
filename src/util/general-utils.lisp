3c7980c29e7d3c063df81561d0fb5ac9354d6172
Marc Kuo <kuomarc2@gmail.com>
2012-04-30 12:41:55 -0700

    add date utilities and macros for convenience

--- src/util/general-utils.lisp
+++ src/util/general-utils.lisp
@@ -1,10 +1,7 @@
 (in-package :example)
 
-(defun print-date (date)
-  "Given a date (postmodern style) decode and return a string"
-  (multiple-value-bind (year month day) (decode-date date)
-    (with-output-to-string (s)
-      (format s "~4,'0d-~2,'0d-~2,'0d" year month day))))
+(defun symb (a b)
+  (intern (format nil "~a-~a" (symbol-name a) (symbol-name b))))
 
 (defun mkstr (obj)
   (if (stringp obj) obj
@@ -19,3 +16,28 @@
   "Drops the table completely, deletes all entries. Use only when redefining models completely."
   (with-connection (db-params)
     (query (conc "DROP TABLE " name))))
+
+;; Simple date utils
+;; -----------------
+(defmacro with-date (date &body body)
+  "Decodes date and binds to year, month and day variables."
+  `(multiple-value-bind (year month day) (decode-date ,date)
+     ,@body))    
+
+(defun print-date (date)
+  "Given a date (postmodern style) decode and return a string"
+  (with-date date
+    (with-output-to-string (s)
+      (format s "~4,'0d-~2,'0d-~2,'0d" year month day))))
+
+(defmacro def-date-getter (element)
+  `(defun ,(symb 'get element) (date)
+     ,(conc "Given a date object, return the " (string element))
+     (with-date date
+       ,element)))
+
+(def-date-getter day)
+(def-date-getter month
+(def-date-getter year)
+
+;; -------------
