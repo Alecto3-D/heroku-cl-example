4d668a5b3d8040fb849a0be392180be709616912
Marc Kuo <kuomarc2@gmail.com>
2012-04-30 12:25:40 -0700

    adjust rating list to use symbols - edit to fill in rating accordingly

--- src/views/edit.lisp
+++ src/views/edit.lisp
@@ -8,7 +8,7 @@
 	   (:label "Title")
 	   (:input :name "movie-title" :size "30" :type "text" :value (movie-title movie))
 	   (:label "Rating")
-	   (selector-form "movie-rating" '("G" "PG" "PG-13" "R" "NC-17"))
+	   (selector-form "movie-rating" '(G PG PG-13 R NC-17) (intern (movie-rating movie)))
 	   (:label "Released on")
 	   (selector-form "year" (num-list 2012 1930))
 	   (selector-form "month" (num-list 1 12))
--- src/views/new.lisp
+++ src/views/new.lisp
@@ -7,7 +7,7 @@
 	   (:label "Title")
 	   (:input :name "movie-title" :size "30" :type "text")
 	   (:label "Rating")
-	   (selector-form "movie-rating" '("G" "PG" "PG-13" "R" "NC-17"))
+	   (selector-form "movie-rating" '(G PG PG-13 R NC-17))
 	   (:label "Released on")
 	   (selector-form "year" (num-list 2012 1930))
 	   (selector-form "month" (num-list 1 12))
